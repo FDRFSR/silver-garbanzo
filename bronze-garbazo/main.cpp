35ca6d5e157227cc53f9ab1346f88e78d745ffef
Federico Fusarri <federicofusarri@macbook-air-di-federico.local>
2018-10-19 13:47:00 +0200

    2018-10-19: input class employees

---
+++ bronze-garbazo/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  bronze-garbazo
+//
+//  Created by Federico Fusarri on 19/10/2018.
+//  Copyright © 2018 Federico Fusarri. All rights reserved.
+//
+
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+class Person {
+    
+private
+    
+    string nome;
+    string cognome;
+    double salario;
+    double orelavoro;
+    double costorario;
+    
+public
+    
+    void set_FieldName();
+    void get_FieldName();
+    void gett_FieldName();
+    void set_Name();
+    void cognomesortLists();
+    void in_nome();
+    void in_cognome();
+    void in_orelavoro();
+    void in_costorario();
+};
+
+    
+    
+}
+
+
+
